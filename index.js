cd0708f8814b75e0b31ac73b42f11cb7fe2b7304
wsavd <tuve@rootfest.net>
2017-11-05 01:46:20 +0300

    c

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
